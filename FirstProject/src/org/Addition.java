d379cee4e5c1fedc78254f17e9af111b8abf747c
shalu0017 <shalu.cie.18.052@gmail.com>
2024-08-04 10:49:55 +0530

    This is my first commit

---
+++ FirstProject/src/org/Addition.java
@@ -0,0 +1,11 @@
+package org;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is my first project");
+	}
+
+}
